190f4fa3fbc8297466b13a4b6571f25ad4f7608d
F. Talha Altınel <talhaaltinel@hotmail.com>
2020-09-29 14:07:47 +0100

    certification logo images

--- components/certification.js
+++ components/certification.js
@@ -1,21 +1,24 @@
 export default function Certification() {
     return (
         <section className="section" id="certification">
-            <div className="container">
-                <div className="level" >
-                    <div className="level-item has-text-centered">
-                        <div>
-                            <p className="aat-title">Member of </p>
-                            <img src="/AAT.png" />                       
-                        </div>
-                    </div>
-                    <div className="level-item has-text-centered">
-                        <div>
-                            <p className="ea-title">Partner of </p>
-                            <img src="/EA.png" />
-                        </div>
+            <div className="columns is-centered is-vcentered">
+                <div className="column is-8">
+                <h2 className="title">
+                    Providing solutions with Business Specialists (PhD), 
+                    Certified Chartered Accountants (ACCE,FCCA) and Qualified Bookkeepers (AAT)
+                </h2>
+                </div>
+                <div className="column is-4 ">
+                    <div className="certification-logo-image-wrapper" >
+                        <div><img src="/ACCA.png" /></div>
+                        <div><img src="/AAT.png" /></div>
+                        <div><img src="/EA.png" /></div>                       
                     </div>
                 </div>
+            </div>
+            <div className="container">
+                
+                
             </div>
         </section>
     );
