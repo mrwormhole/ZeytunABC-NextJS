d6c432930204a28d61c652deb76c8ff8f7813d9d
Talha Altınel <talhaaltinel@hotmail.com>
2022-06-13 01:04:20 +0100

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
-import '@fortawesome/fontawesome-svg-core/styles.css'
-import "../styles/_app.scss"
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import "../styles/_app.scss";
 
+import Script from 'next/script';
 import { useState } from 'react';
 import LoadingContext from '../components/loadingContext';
 import { CookiesProvider, useCookies } from 'react-cookie';
@@ -30,6 +31,17 @@ export default function App({Component, pageProps}) {
         <LoadingContext.Provider value = {{
             isLoading: isLoading,
             setIsLoading: setIsLoading}}>
+                <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+                <Script strategy="lazyOnload">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                        page_path: window.location.pathname,
+                        });
+                    `}
+                </Script>
                 <Component {...pageProps} />
         </LoadingContext.Provider>
     </CookiesProvider>
